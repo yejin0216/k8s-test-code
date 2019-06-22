389f9925a842889a707cc7e1a74a732279f5d2cd
yejinkim <yejin0216@gmail.com>
2019-06-22 22:59:34 +0900

    불필요한 파일 삭제

--- pokemonsList.js
+++
@@ -1,9 +0,0 @@
-var os = require('os');
-
-var http = require('http');
-var handleRequest = function (request, response) {
-  response.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-  response.end("전체 포켓몬 목록을 보여주는 화면입니다.");
-}
-var www = http.createServer(handleRequest);
-www.listen(8080);
